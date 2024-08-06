c7929df118ba6786cc66ee2c1d412ea493ecc0e5
gonzatorte <gonzatorte@gmail.com>
2024-08-05 21:45:48 -0300

    cleanup

--- onto/generator/ontologies/repo.py
+++ onto/generator/ontologies/repo.py
@@ -1,117 +1,118 @@
 from .base import onto as base_onto, initiate as base_initiate
-from owlready2 import *
+import owlready2 as ow
+# from owlready2 import *
 
-onto = get_ontology("http://test.org/repo.owl")
+onto = ow.get_ontology("http://test.org/repo.owl")
 
 
 def initiate():
     base_initiate()
     with onto:
-        class politica(Thing):
+        class politica(ow.Thing):
             pass
         politica.nombre_usual = [
-            locstr("política", lang="es"),
-            locstr("policy", lang="en"),
+            ow.locstr("política", lang="es"),
+            ow.locstr("policy", lang="en"),
         ]
 
-        class repositorio(Thing):
+        class repositorio(ow.Thing):
             pass
         repositorio.nombre_usual = [
-            locstr("repositorio", lang="es"),
-            locstr("repository", lang="en"),
+            ow.locstr("repositorio", lang="es"),
+            ow.locstr("repository", lang="en"),
         ]
         repositorio.sinonimo = [
-            locstr("archivo", lang="es"),
-            locstr("archive", lang="en"),
+            ow.locstr("archivo", lang="es"),
+            ow.locstr("archive", lang="en"),
         ]
 
-        class motor_de_repositorio(Thing):
+        class motor_de_repositorio(ow.Thing):
             pass
 
-        class agregador(Thing):
+        class agregador(ow.Thing):
             pass
         agregador.nombre_usual = [
-            locstr("agregador", lang="es"),
-            locstr("aggregator", lang="en"),
+            ow.locstr("agregador", lang="es"),
+            ow.locstr("aggregator", lang="en"),
         ]
         agregador.sinonimo = [
-            locstr("buscador", lang="es"),
+            ow.locstr("buscador", lang="es"),
         ]
 
-        class catalogo_de_repositorio(Thing):
+        class catalogo_de_repositorio(ow.Thing):
             pass
         catalogo_de_repositorio.nombre_usual = [
-            locstr("catalogo de repositorios", lang="es"),
-            locstr("repository_catalog", lang="en"),
+            ow.locstr("catalogo de repositorios", lang="es"),
+            ow.locstr("repository_catalog", lang="en"),
         ]
         catalogo_de_repositorio.sinonimo = [
-            locstr("proveedor de servicio", lang="en"),
+            ow.locstr("proveedor de servicio", lang="en"),
         ]
 
-        class funcionalidad(Thing):
+        class funcionalidad(ow.Thing):
             pass
         funcionalidad.nombre_usual = [
-            locstr("funcionalidad", lang="es"),
-            locstr("feature", lang="en"),
+            ow.locstr("funcionalidad", lang="es"),
+            ow.locstr("feature", lang="en"),
         ]
 
         class funcionalidad_para_cdd(funcionalidad):
             pass
         funcionalidad_para_cdd.nombre_usual = [
-            locstr("funcionalidad para colecciones", lang="es"),
-            locstr("collection feature", lang="en"),
+            ow.locstr("funcionalidad para colecciones", lang="es"),
+            ow.locstr("collection feature", lang="en"),
         ]
 
         class exportacion_de_citas(funcionalidad):
             pass
         exportacion_de_citas.nombre_usual = [
-            locstr("exportación de citas", lang="es"),
-            locstr("cite export", lang="en"),
+            ow.locstr("exportación de citas", lang="es"),
+            ow.locstr("cite export", lang="en"),
         ]
 
         class api_para_cosecha(funcionalidad):
             pass
         api_para_cosecha.nombre_usual = [
-            locstr("api para cosecha", lang="es"),
-            locstr("harvesting api", lang="en"),
+            ow.locstr("api para cosecha", lang="es"),
+            ow.locstr("harvesting api", lang="en"),
         ]
 
         class control_de_acceso(funcionalidad):
             pass
         control_de_acceso.nombre_usual = [
-            locstr("control de acceso", lang="es"),
-            locstr("access control", lang="en"),
+            ow.locstr("control de acceso", lang="es"),
+            ow.locstr("access control", lang="en"),
         ]
 
         class integracion_con_red_social(funcionalidad):
             pass
         integracion_con_red_social.nombre_usual = [
-            locstr("integración con red social", lang="es"),
-            locstr("social media integration", lang="en"),
+            ow.locstr("integración con red social", lang="es"),
+            ow.locstr("social media integration", lang="en"),
         ]
 
         class interfaz_multilenguaje(funcionalidad):
             pass
         interfaz_multilenguaje.nombre_usual = [
-            locstr("interfaz multilenguaje", lang="es"),
-            locstr("multilanguage interface", lang="en"),
+            ow.locstr("interfaz multilenguaje", lang="es"),
+            ow.locstr("multilanguage interface", lang="en"),
         ]
 
         class periodo_de_embargo(funcionalidad):
             pass
         periodo_de_embargo.nombre_usual = [
-            locstr("período de embargo", lang="es"),
-            locstr("embargo period", lang="en"),
+            ow.locstr("período de embargo", lang="es"),
+            ow.locstr("embargo period", lang="en"),
         ]
 
         class servicio_de_curaduria(funcionalidad):
             pass
         servicio_de_curaduria.nombre_usual = [
-            locstr("servicio de curaduría", lang="es"),
-            locstr("curator service", lang="en"),
+            ow.locstr("servicio de curaduría", lang="es"),
+            ow.locstr("curator service", lang="en"),
         ]
 
-        AllDisjoint([
+        ow.AllDisjoint([
             exportacion_de_citas,
             api_para_cosecha,
             control_de_acceso,
@@ -127,7 +128,7 @@ def initiate():
         class caracteristica_de_productor_de_datos(soporte_para_caracteristica_de_cdd):
             pass
 
-        class utiliza_motor(ObjectProperty, FunctionalProperty):
+        class utiliza_motor(ow.ObjectProperty, ow.FunctionalProperty):
             domain = [repositorio]
             range = [motor_de_repositorio]
 
@@ -139,36 +140,36 @@ def initiate():
 
         # ToDo: quizas es mejor modelar que la relacion es comercial cuando la organizacion con la que se relaciona es un financiador
         # ToDo: quizas es mejor modelar esto como un concepto y es mejor para traducirlo
-        class tipo_de_relacion_con_organizacion(Datatype):
-            equivalent_to = [OneOf(["financiamiento", "técnica", "administrativa"])]
+        class tipo_de_relacion_con_organizacion(ow.Datatype):
+            equivalent_to = [ow.OneOf(["financiamiento", "técnica", "administrativa"])]
 
-        class tiene_tipo_de_relacion_con_organizacion(DataProperty, FunctionalProperty):
+        class tiene_tipo_de_relacion_con_organizacion(ow.DataProperty, ow.FunctionalProperty):
             domain = [relacionado_con_organizacion]
             range = [tipo_de_relacion_con_organizacion]
 
-        class tiene_periodo_de_relacion_con_organizacion(DataProperty, FunctionalProperty):
+        class tiene_periodo_de_relacion_con_organizacion(ow.DataProperty, ow.FunctionalProperty):
             domain = [relacionado_con_organizacion]
             # ToDo: Buscar un datatype nuevo que permita declarar cosas como intervalo entre fechas (y fechas con extremos indeterminados)
             #  tb permitir expresar "desde una fecha pero no se sabe hasta cuándo" como algo distinto de "desde una fecha hasta el infinito"
             range = [str]
 
-        class tiene_nombre_politica(Datatype):
+        class tiene_nombre_politica(ow.Datatype):
             domain = [politica]
             range = [str]
 
-        class tiene_url_politica(Datatype):
+        class tiene_url_politica(ow.Datatype):
             domain = [politica]
             # ToDo: Usar foaf:Document para referenciar a manuales o cosas que deben ser leídas por un humano
             range = [str]
 
         # ToDo: Quizas hacerlo un FunctionalProperty y que la politica sea un conjunto de condiciones
-        class usa_politica(ObjectProperty):
+        class usa_politica(ow.ObjectProperty):
             domain = [relacionado_con_organizacion]
             range = [politica]
 
         # ToDo: En realidad, un repositorio puede soportar más de 1 lenguaje... así que no es FunctionalProperty
         #  aunque el manejo de colecciones puede generar problemas para restringir la unicidad en esas colecciones (aunque no sé si importa)
-        class usa_lenguaje(DataProperty, FunctionalProperty):
+        class usa_lenguaje(ow.DataProperty, ow.FunctionalProperty):
             domain = [repositorio]
             range = [base_onto.lenguaje]
         # class usa_lenguaje(repositorio >> base_onto.lenguaje):
@@ -178,15 +179,14 @@ def initiate():
             pass
 
         # ToDo: No se está diciendo el identificador en que sistema... tomamos re3data como el estándar?
-        class id_de_repositorio(DataProperty, FunctionalProperty):
+        class id_de_repositorio(ow.DataProperty, ow.FunctionalProperty):
             domain = [es_catalogado_por]
             range = [str]
 
 
 if __name__ == '__main__':
     initiate()
-    import owlready2
-    owlready2.JAVA_EXE = "/usr/bin/java"
-    owlready2.sync_reasoner()
-    if len(list(owlready2.default_world.inconsistent_classes())) != 0:
+    ow.JAVA_EXE = "/usr/bin/java"
+    ow.sync_reasoner()
+    if len(list(ow.default_world.inconsistent_classes())) != 0:
         raise Exception('Inconsistent ontology')
